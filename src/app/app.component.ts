8a99652ddae8816d487dae543c54a6a7be6bfb41
mir20000 <mir20000@users.noreply.github.com>
2022-01-20 18:49:11 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountserviceService } from './account/services/accountservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'final-app';
+  constructor(
+    public router: Router,
+
+    private accountService: AccountserviceService
+  ) {}
+  title = 'frontend';
+  ngOnInit(): void {
+    this.accountService.autoAuthUser();
+  }
 }
